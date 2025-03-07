81f51b468a3aab3dce0518ba56ba44abaa3ac5e2
FanFusheng <ffs@ppno.net>
2025-03-08 00:09:29 +0800

    feat(mcp-time): 添加当前时间工具
    - 新增 MCP Time 工具，提供获取当前时间的功能
    - 添加了对时区参数的支持，默认为 Asia/Shanghai
    - 引入了 mark3labs/mcp-go 和 google/uuid 依赖

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/fsyyft-ai/mcp-time
 
 go 1.24
+
+require github.com/mark3labs/mcp-go v0.11.2
+
+require github.com/google/uuid v1.6.0 // indirect
--- internal/app/mcp-time/app.go
+++ internal/app/mcp-time/app.go
@@ -5,9 +5,45 @@
 package mcp_time
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
 )
 
 func Run() {
-	fmt.Println("Hello, World!")
+	// Create MCP server
+	s := server.NewMCPServer(
+		"MCP Time",
+		"0.0.1",
+	)
+	// Add tool
+	tool := mcp.NewTool("current time",
+		mcp.WithDescription("Get current time with timezone, Asia/Shanghai is default"),
+		mcp.WithString("timezone",
+			mcp.Required(),
+			mcp.Description("current time timezone"),
+		),
+	)
+	// Add tool handler
+	s.AddTool(tool, currentTimeHandler)
+	// Start the stdio server
+	if err := server.ServeStdio(s); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
+}
+
+func currentTimeHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	timezone, ok := request.Params.Arguments["timezone"].(string)
+	if !ok {
+		return mcp.NewToolResultError("timezone must be a string"), nil
+	}
+
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		return mcp.NewToolResultError(fmt.Sprintf("parse timezone with error: %v", err)), nil
+	}
+	return mcp.NewToolResultText(fmt.Sprintf(`current time is %s`, time.Now().In(loc))), nil
 }
